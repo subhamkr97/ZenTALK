27fc9f5256a2fbe33881d05f3fd8e15ccf03978d
Subham Kumar <subhkum@iitk.ac.in>
2023-01-31 03:13:10 +0530

    Initial Commit

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome({currentUser}) {
+  return (
+    <Container>
+        <img src={Robot} alt="Robot" />
+        <h1>
+            Welcome, <span>{currentUser.username}!</span>
+        </h1>
+        <h3>Start messaging your friends.</h3>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
